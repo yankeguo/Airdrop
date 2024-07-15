9c121d26c9a87065d2015f67a86859ca87091267
GUO YANKE <hi@yankeguo.com>
2024-07-15 17:52:47 +0800

    chore(ui): update title

--- components/index/title.vue
+++ components/index/title.vue
@@ -1,13 +1,23 @@
+<script setup lang="ts">
+import { YGTOG } from "@yankeguo/ygtog";
+
+const scanURL = computed(() => {
+  if (YGTOG.contract.chain === "gnosis") {
+    return `https://gnosis.blockscout.com/token/${YGTOG.contract.address}`;
+  }
+  return "https://yankeguo.com";
+});
+</script>
+
 <template>
   <div class="flex flex-col gap-2 lg:gap-3 font-bold">
     <a
       class="flex flex-row items-center text-xl lg:text-2xl"
-      href="https://gnosis.blockscout.com/token/0xc9174F37f7C969e26d91C0A6001c424f1426c6bD"
+      :href="scanURL"
       target="_blank"
     >
       <img src="~/assets/token-icon.svg" class="w-6 lg:w-8 ms-2 me-1" />
-      <span class="text-lime-400 ms-1">YGTOG</span>
+      <span class="text-lime-400 ms-1">Token of Gratitude by Yanke Guo</span>
     </a>
-    <span class="ms-2 text-lg lg:text-xl">Token of Gratitude by Yanke Guo</span>
   </div>
 </template>
