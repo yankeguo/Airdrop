ebba6f3b8b922b954ae6669b97ff3ecfaf480336
GUO YANKE <hi@yankeguo.com>
2024-07-15 18:03:37 +0800

    chore(ui): update favicon

--- app.vue
+++ app.vue
@@ -1,8 +1,19 @@
+<script setup lang="ts">
+import linkAssetsIcon32x32 from "~/assets/icon-32x32.png";
+</script>
+
 <template>
   <div>
     <Html>
       <Head>
         <Title>Token of Gratitude by Yanke Guo</Title>
+        <Link
+          rel="icon"
+          type="image/png"
+          size="32x32"
+          :href="linkAssetsIcon32x32"
+        >
+        </Link>
       </Head>
     </Html>
     <NuxtRouteAnnouncer />
