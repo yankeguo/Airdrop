f11600b420cf3a587a56b7a1ddb63a88ae580cce
GUO YANKE <hi@yankeguo.com>
2024-07-15 18:13:33 +0800

    chore(ui): update redirect url

--- pages/oauth/github/callback.vue
+++ pages/oauth/github/callback.vue
@@ -22,7 +22,7 @@ onMounted(async () => {
 
 <template>
   <div
-    class="w-screen h-screen flex flex-row justify-center items-center font-bold text-xl lg:text-3xl"
+    class="w-screen h-screen flex flex-row justify-center items-center font-semibold text-lg lg:text-xl"
   >
     <span>Please wait while we are redirecting you back...</span>
   </div>
